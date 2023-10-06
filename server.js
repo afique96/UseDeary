b6b4f403387babe0009a7e983854c89c273c5aba
Akifcan Özbulut <56379702+Afikcan@users.noreply.github.com>
2023-10-06 17:45:02 +0300

    deploy

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/build")));
+}
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 
 app.use("/plan", require("./routes/plan"));
 app.use("/user", require("./routes/user"))
 app.use("/auth", require("./routes/auth"))
+
+if (process.env.NODE_ENV === "production") {
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    });
+}
