f132c24b831bbe9942699a85013b87eab3c3e9ac
Akifcan Özbulut <56379702+Afikcan@users.noreply.github.com>
2023-10-06 17:51:28 +0300

    fix forgeting require("path");

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
